278d45c0121d354252d03a4310e064b52413e1cb
Danny Hughes <dghughes84@gmail.com>
2023-03-17 15:25:27 -0600

    c

--- Sushi_stables_app/config.py
+++ Sushi_stables_app/config.py
@@ -9,3 +9,5 @@ print(f"ShroomDK API Key: {shroomdk_key}", flush=True)
 # In the main app file
 print(f"Initializing ShroomSDK with API Key: {shroomdk_key}", flush=True)
 
+print("Environment Variables:", flush=True)
+print(os.environ, flush=True)
