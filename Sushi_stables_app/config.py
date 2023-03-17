378dcbc1bb116b021435d5ba6656821dcfa7e767
Danny Hughes <dghughes84@gmail.com>
2023-03-17 15:23:33 -0600

    c

--- Sushi_stables_app/config.py
+++ Sushi_stables_app/config.py
@@ -1,5 +1,11 @@
 import os
-
+import sys
 shroomdk_key = os.environ.get('shroomdk_key')
 
 print(f"ShroomDK API Key: {shroomdk_key}")
+# In your config.py
+print(f"ShroomDK API Key: {shroomdk_key}", flush=True)
+
+# In the main app file
+print(f"Initializing ShroomSDK with API Key: {shroomdk_key}", flush=True)
+
